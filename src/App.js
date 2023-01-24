710c631cea123511bcb501bd092a75e17d1ee11e
Alanmp1857 <alan.panicker-bvcoel@bvp.edu.in>
2023-01-24 16:12:20 +0530

    4th commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       {/* <Navbar /> */}
       <div className="container">
         <TextForm heading="Enter the text to analyze" />
+        {/* <About /> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -22,6 +22,17 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
+    const handleCopy = (event) => {
+        let text=document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = (event) => {
+        let newText=text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+
     const [text, setText]=useState('');
     return (
         <>
@@ -34,6 +45,8 @@ export default function TextForm(props) {
             <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
             <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear text</button>
+            <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy text</button>
+            <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
         <div className="container my-3">
             <h2>Your text summary</h2>
