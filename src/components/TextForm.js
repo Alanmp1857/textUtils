7b5e59b813c568ab273b9e081e3202281b621a36
Alanmp1857 <alan.panicker-bvcoel@bvp.edu.in>
2023-01-24 11:37:37 +0530

    3rd commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,6 +7,16 @@ export default function TextForm(props) {
         setText(newText)
     }
 
+    const handleLoClick = () => {
+        const newText=text.toLowerCase();
+        setText(newText)
+    }
+
+    const handleClearClick = () => {
+        const newText='';
+        setText(newText)
+    }
+
     const handleOnChange = (event) => {
         // console.log("On Change")
         setText(event.target.value);
@@ -14,13 +24,24 @@ export default function TextForm(props) {
 
     const [text, setText]=useState('');
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 {/* {props.heading} */}
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear text</button>
         </div>
+        <div className="container my-3">
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
